3d304111ef80508f2f26eb0eea9b1ab2d6ce176c
mohamdwiners@gmail.com <mohamdwiners@gmail.com>
2026-05-03 11:29:26 +0100

    FIX method DELETE

--- include/client/Client.hpp
+++ include/client/Client.hpp
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-# pragma once
-
-
-
-# include "CgiClient.hpp"
-
-#include <algorithm>
-
-
-enum ParseState {
-    READING_HEADERS,
-    READING_BODY,
-    COMPLETE,
-    ERROR
-};
-
-
-class Client // needs config file .
-{
-public:
-    // server config
-    int                 http_fd; // UPDATE
-
-    // struct pollfd&      pfd; // client now has pfd. // DANGER 
-    
-    const ServerConfig& serverConfig;
-    int                 clientPort;
-    std::string         clientAddress;
-    
-    // request parsing
-    std::string request_str;
-    HttpRequest request;
-    ParseState  state;
-    size_t      header_end_pos;
-    std::string header_str;
-    
-    // cgi 
-    bool                isCgi;
-    bool                isCgiResponseError;
-    Cgi*                cgi;
-
-    
-    // response
-    std::string         response_str;
-    HttpResponse        response;
-    size_t              bytes_sent;
-    // needs response buffering , send()
-    
-    Client(const ServerConfig& serverConfig, int clientPort, std::string clientAddress);
-    ~Client();
-    // Client& operator=(const Client& newClient);
-    void    parseRequest();
-    void    parseRequestHeaders();
-    void    parseRequestBody();
-};
-
-=======
 # pragma once
 
 
@@ -113,5 +55,4 @@ public:
     void    parseRequestBody();
 };
 
->>>>>>> b20e531b0729fb7ef7f70ca15dcf21f0d92cb540
 std::string trim(const std::string& str);
