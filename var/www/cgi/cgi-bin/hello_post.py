0a4dc052ebeb16170ba7222fa1c9acc0adfe142f
Adnane kharkhour <adnangamas@gmail.com>
2026-05-03 18:46:52 +0100

    fix: Error pages config

--- var/www/cgi/cgi-bin/hello_post.py
+++ var/www/cgi/cgi-bin/hello_post.py
@@ -7,19 +7,19 @@ body = sys.stdin.read() or ""
 params = urllib.parse.parse_qs(body, keep_blank_values=True)
 
 name = params.get("name", [""])[0]
-city = params.get("city", [""])[0]
+email = params.get("email", [""])[0]
 
 print("Content-Type: text/plain")
 print()
-print(f"HELLO {name} from {city} CGI")
+print(f"HELLO {name} from {email} CGI")
 print("Starting CGI process...")
 # while(1)
 #     continue
-print(f"BYE {name} from {city} CGI")
+print(f"BYE {name} from {email} CGI")
 # Command to test
 #  curl -v -X POST \
 #   -H "Content-Type: application/x-www-form-urlencoded" \
-#   -d "name=alice&city=rabat" \
+#   -d "name=alice&email=rabat@example.com" \
 #   http://localhost:4242/cgi/hello_post.py
 
 
