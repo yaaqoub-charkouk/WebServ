397cf006958f8073d207d388fe08c90e311c366d
Adnane <adnanekharkhour@gmail.com>
2026-05-04 01:18:10 +0100

    fix: fixed a bug in cgi and added the evaluation report

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Compiler and flags
 CXX = c++
-CXXFLAGS = -Wall -Wextra -Werror -std=c++98 -fsanitize=address
+CXXFLAGS = -Wall -Wextra -Werror -std=c++98 #-fsanitize=address
 INCLUDES = -Iinclude
 
 # Directories
--- src/cgi/Cgi.cpp
+++ src/cgi/Cgi.cpp
@@ -322,7 +322,8 @@ void    Cgi::buildEnvp(const HttpRequest &req)
     env_vect.push_back("SERVER_SOFTWARE=WebServ/1.0");
     env_vect.push_back(("SERVER_PORT=" + ultostr(req.serverPort)));
     env_vect.push_back("SERVER_NAME=" + req.serverName);
-    env_vect.push_back("COOKIE=" + req.headers.at("cookie"));
+    if (req.headers.count("cookie"))
+        env_vect.push_back("COOKIE=" + req.headers.at("cookie"));
 
     buildHeaders(req);
 
